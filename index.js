81edfbb191528fd969ddebdd9cb545f65530f466
stregaa <markus_chung@hotmail.com>
2022-12-11 20:00:13 -0800

    ✨ added basic navbar partial, changed routing

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ const app = express();
 app.use(ejsLayouts);
 app.set("view engine", "ejs");
 
+// Default page
 app.get("/", function(req, res){
-    res.send("default page, maybe login?")
+    res.render("layout")
 });
 
+// Recipes list (doesn't exist yet)
 app.get("/recipe", function(req, res){
-    res.render("layout")
+    res.render("recipes")
 })
 
-//localhost:8080
-app.listen(8080);
+// Login page (doesn't exist yet)
+app.get("/login", function(req, res){
+    res.render("login")
+})
 
+// localhost:8080
+app.listen(8080);
